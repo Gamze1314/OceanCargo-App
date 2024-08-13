65f98460309cb3b18096a8568933ef7e4e7015ff
gamze <gamzekandara@gmail.com>
2024-08-13 19:27:56 -0400

    installs Flask restful, migrate and sqlalchemy

---
+++ server/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, make_response, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Api, Resource
+
+from flask_migrate import Migrate
+
+# from models import db
+
+# create a Flask application object
+app = Flask(__name__)
+
+# configure a database connection to the local file app.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+# disable modification tracking to use less memory
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# create a Migrate object to manage schema modifications
+# migrate = Migrate(app, db)
+
+# # initialize the Flask application to use the database
+# db.init_app(app)
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
